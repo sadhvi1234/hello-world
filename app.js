48549902d7099cc831c31c23369ae37202ff3f0e
sadhvi1234 <58354621+sadhvi1234@users.noreply.github.com>
2020-07-23 02:10:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,269 +1,148 @@
-function init(player, OPPONENT){
-    // SELECT CANAVS
-    const canvas = document.getElementById("cvs");
-    const ctx = canvas.getContext("2d");
+//HTML Elements
 
-    // BOARD VARIABLES
-    let board = [];
-    const COLUMN = 3;
-    const ROW = 3;
-    const SPACE_SIZE = 150;
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell');
 
-    // STORE PLAYER'S MOVES
-    let gameData = new Array(9);
-    
-    // By default the first player to play is the human
-    let currentPlayer = player.man;
 
-    // load X & O images
-    const xImage = new Image();
-    xImage.src = "img/X.png";
+//game constants
+const xSymbol = '×';
+const oSymbol = '○';
 
-    const oImage = new Image();
-    oImage.src = "img/O.png";
 
-    // Win combinations
-    const COMBOS = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+//game variables
+let gameislive = true;
+let xisnext=true;
+let winner = null;
 
-    // FOR GAME OVER CHECK
-    let GAME_OVER = false;
-    
-    // DRAW THE BOARD
-    function drawBoard(){
-        // WE give every space a unique id
-        // So we know exactly where to put the player's move on the gameData Array
-        let id = 0
-        for(let i = 0; i < ROW; i++){
-            board[i] = [];
-            for(let j = 0; j < COLUMN; j++){
-                board[i][j] = id;
-                id++;
 
-                // draw the spaces
-                ctx.strokeStyle = "#000";
-                ctx.strokeRect(j * SPACE_SIZE, i * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
-            }
+//functions
+const lettertosymbol = (letter) => letter === 'x' ? xSymbol:oSymbol;
+
+
+const handlewin =(letter) =>{
+    gameislive=false;
+    winner=letter;
+    if(winner ==='x'){ 
+        statusDiv.innerHTML=`${lettertosymbol(winner)} has won!`;
+    }
+    else {
+        statusDiv.innerHTML = `<span>${lettertosymbol(winner)} has won!</span>`;
+    }
+}
+
+
+const checkgamestatus=()=>{
+    const topleft = cellDivs[0].classList[1];
+    const topmiddle = cellDivs[1].classList[1];
+    const topright = cellDivs[2].classList[1];
+    const middleleft = cellDivs[3].classList[1];
+    const middlemiddle = cellDivs[4].classList[1];
+    const middleright = cellDivs[5].classList[1];
+    const bottomleft = cellDivs[6].classList[1];
+    const bottommiddle = cellDivs[7].classList[1];
+    const bottomright = cellDivs[8].classList[1];
+
+
+    // is there a winner?
+    if(topleft && topleft===topmiddle && topleft===topright){
+        handlewin(topleft);
+        cellDivs[0].classList.add('Won');
+        cellDivs[1].classList.add('Won');
+        cellDivs[2].classList.add('Won');
+    } 
+    else if(middleleft && middleleft === middlemiddle && middleleft === middleright){
+        handlewin(middleleft);
+        cellDivs[3].classList.add('Won');
+        cellDivs[4].classList.add('Won');
+        cellDivs[5].classList.add('Won');
+    }
+    else if(bottomleft && bottomleft ===bottommiddle && bottomleft ===bottomright){
+        handlewin(bottomleft);
+        cellDivs[6].classList.add('Won');
+        cellDivs[7].classList.add('Won');
+        cellDivs[8].classList.add('Won');
+    }
+    else if(topleft && topleft ===middleleft && topleft===bottomleft){
+        handlewin(topleft);
+        cellDivs[0].classList.add('Won');
+        cellDivs[3].classList.add('Won');
+        cellDivs[6].classList.add('Won');
+    }
+    else if(topmiddle && topmiddle===middlemiddle && topmiddle===bottommiddle){
+        handlewin(topmiddle);
+        cellDivs[1].classList.add('Won');
+        cellDivs[4].classList.add('Won');
+        cellDivs[7].classList.add('Won');
+    }
+    else if(topright && topright===middleright && topright===bottomright){
+        handlewin(topright);
+        cellDivs[2].classList.add('Won');
+        cellDivs[5].classList.add('Won');
+        cellDivs[8].classList.add('Won');
+    }
+    else if(topleft && topleft===middlemiddle && topleft===bottomright){
+        handlewin(topleft);
+        cellDivs[0].classList.add('Won');
+        cellDivs[4].classList.add('Won');
+        cellDivs[8].classList.add('Won');
+    }
+    else if(topright && topright===middlemiddle && topright===bottomleft){
+        handlewin(topright);
+        cellDivs[2].classList.add('Won');
+        cellDivs[4].classList.add('Won');
+        cellDivs[6].classList.add('Won');
+    }
+    else if(topleft && topmiddle && topright && middlemiddle && middleright && middleleft && bottomleft && bottommiddle && bottomright){
+        gameislive=false;
+        statusDiv.innerHTML='Game is tied!';
+    }
+    else{
+        xisnext = !xisnext;
+        if(xisnext){
+            statusDiv.innerHTML = `${xSymbol} is next`;
+        }
+        else{
+            statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
         }
     }
-    drawBoard();
+};
 
-    // ON PLAYER'S CLICK
-    canvas.addEventListener("click", function(event){
-        
-        // IF IT's A GAME OVER? EXIT
-        if(GAME_OVER) return;
 
-        // X & Y position of mouse click relative to the canvas
-        let X = event.clientX - canvas.getBoundingClientRect().x;
-        let Y = event.clientY - canvas.getBoundingClientRect().y;
+// event handelers
+const handleReset = () => {
+    xisnext = true;
+    gameislive=true;
+    statusDiv.innerHTML = `${xSymbol} is next`;
+    winner=null;
+    for(const cellDiv of cellDivs){
+        cellDiv.classList.remove('x');
+        cellDiv.classList.remove('o');
+        cellDiv.classList.remove('won');
+    }
+};
 
-        // WE CALCULATE i & j of the clicked SPACE
-        let i = Math.floor(Y/SPACE_SIZE);
-        let j = Math.floor(X/SPACE_SIZE);
+const handlecellclick = (e) => {
+    const classList = e.target.classList;
 
-        // Get the id of the space the player clicked on
-        let id = board[i][j];
-
-        // Prevent the player to play the same space twice
-        if(gameData[id]) return;
-
-        // store the player's move to gameData
-        gameData[id] = currentPlayer;
-        
-        // draw the move on board
-        drawOnBoard(currentPlayer, i, j);
-
-        // Check if the play wins
-        if(isWinner(gameData, currentPlayer)){
-            showGameOver(currentPlayer);
-            GAME_OVER = true;
-            return;
-        }
-
-        // check if it's a tie game
-        if(isTie(gameData)){
-            showGameOver("tie");
-            GAME_OVER = true;
-            return;
-        }
-
-        if( OPPONENT == "computer"){
-            // get id of space using minimax algorithm
-            let id = minimax( gameData, player.computer ).id;
-
-            // store the player's move to gameData
-            gameData[id] = player.computer;
-            
-            // get i and j of space
-            let space = getIJ(id);
-
-            // draw the move on board
-            drawOnBoard(player.computer, space.i, space.j);
-
-            // Check if the play wins
-            if(isWinner(gameData, player.computer)){
-                showGameOver(player.computer);
-                GAME_OVER = true;
-                return;
-            }
-
-            // check if it's a tie game
-            if(isTie(gameData)){
-                showGameOver("tie");
-                GAME_OVER = true;
-                return;
-            }
-        }else{
-            // GIVE TURN TO THE OTHER PLAYER
-            currentPlayer = currentPlayer == player.man ? player.friend : player.man;
-        }
-
-    });
-
-    // MINIMAX
-    function minimax(gameData, PLAYER){
-        // BASE
-        if( isWinner(gameData, player.computer) ) return { evaluation : +10 };
-        if( isWinner(gameData, player.man)      ) return { evaluation : -10 };
-        if( isTie(gameData)                     ) return { evaluation : 0 };
-
-        // LOOK FOR EMTY SPACES
-        let EMPTY_SPACES = getEmptySpaces(gameData);
-
-        // SAVE ALL MOVES AND THEIR EVALUATIONS
-        let moves = [];
-
-        // LOOP OVER THE EMPTY SPACES TO EVALUATE THEM
-        for( let i = 0; i < EMPTY_SPACES.length; i++){
-            // GET THE ID OF THE EMPTY SPACE
-            let id = EMPTY_SPACES[i];
-
-            // BACK UP THE SPACE
-            let backup = gameData[id];
-
-            // MAKE THE MOVE FOR THE PLAYER
-            gameData[id] = PLAYER;
-
-            // SAVE THE MOVE'S ID AND EVALUATION
-            let move = {};
-            move.id = id;
-            // THE MOVE EVALUATION
-            if( PLAYER == player.computer){
-                move.evaluation = minimax(gameData, player.man).evaluation;
-            }else{
-                move.evaluation = minimax(gameData, player.computer).evaluation;
-            }
-
-            // RESTORE SPACE
-            gameData[id] = backup;
-
-            // SAVE MOVE TO MOVES ARRAY
-            moves.push(move);
-        }
-
-        // MINIMAX ALGORITHM
-        let bestMove;
-
-        if(PLAYER == player.computer){
-            // MAXIMIZER
-            let bestEvaluation = -Infinity;
-            for(let i = 0; i < moves.length; i++){
-                if( moves[i].evaluation > bestEvaluation ){
-                    bestEvaluation = moves[i].evaluation;
-                    bestMove = moves[i];
-                }
-            }
-        }else{
-            // MINIMIZER
-            let bestEvaluation = +Infinity;
-            for(let i = 0; i < moves.length; i++){
-                if( moves[i].evaluation < bestEvaluation ){
-                    bestEvaluation = moves[i].evaluation;
-                    bestMove = moves[i];
-                }
-            }
-        }
-
-        return bestMove;
+    if(!gameislive ||  classList[1] === 'x' || classList[1] === 'o'){
+        return;
     }
 
-    // GET EMPTY SPACES
-    function getEmptySpaces(gameData){
-        let EMPTY = [];
-
-        for( let id = 0; id < gameData.length; id++){
-            if(!gameData[id]) EMPTY.push(id);
-        }
-
-        return EMPTY;
+    if(xisnext){
+        e.target.classList.add('x');
+        checkgamestatus();
+    } else {
+        e.target.classList.add('o');
+        checkgamestatus();
     }
+};
 
-    // GET i AND j of a SPACE
-    function getIJ(id){
-        for(let i = 0; i < board.length; i++){
-            for(let j = 0; j < board[i].length; j++){
-                if(board[i][j] == id) return { i : i, j : j}
-            }
-        }
-    }
 
-    // check for a winner
-    function isWinner(gameData, player){
-        for(let i = 0; i < COMBOS.length; i++){
-            let won = true;
 
-            for(let j = 0; j < COMBOS[i].length; j++){
-                let id = COMBOS[i][j];
-                won = gameData[id] == player && won;
-            }
+//event listeners
+resetDiv.addEventListener('click', handleReset);
 
-            if(won){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Check for a tie game
-    function isTie(gameData){
-        let isBoardFill = true;
-        for(let i = 0; i < gameData.length; i++){
-            isBoardFill = gameData[i] && isBoardFill;
-        }
-        if(isBoardFill){
-            return true;
-        }
-        return false;
-    }
-
-    // SHOW GAME OVER
-    function showGameOver(player){
-        let message = player == "tie" ? "Oops No Winner" : "The Winner is";
-        let imgSrc = `img/${player}.png`;
-
-        gameOverElement.innerHTML = `
-            <h1>${message}</1>
-            <img class="winner-img" src=${imgSrc} </img>
-            <div class="play" onclick="location.reload()">Play Again!</div>
-        `;
-
-        gameOverElement.classList.remove("hide");
-    }
-
-    // draw on board
-    function drawOnBoard(player, i, j){
-        let img = player == "X" ? xImage : oImage;
-
-        // the x,y positon of the image are the x,y of the clicked space
-        ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE);
-    }
+for(const cellDiv of cellDivs){
+    cellDiv.addEventListener('click', handlecellclick)
 }
